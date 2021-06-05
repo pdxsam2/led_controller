30f00e4ca31a4711e0c134ca0657e983254a9d36
pdxsam2 <samp2@pdx.edu>
2021-06-05 14:08:15 -0700

    added a couple other modes

--- src/main.rs
+++ src/main.rs
@@ -6,6 +6,7 @@ use panic_halt as _;
 //  clean up imports
 //  README and docs
 //  Organize things into functions?
+//  appease clippy
 use core::mem::MaybeUninit;
 use core::sync::atomic::{AtomicU8, Ordering};
 use cortex_m_rt::entry;
@@ -14,7 +15,7 @@ use stm32f1xx_hal::{
     delay::Delay,
     gpio::*,
     pac,
-    pac::Interrupt::EXTI9_5,
+    pac::Interrupt::{EXTI4, EXTI9_5},
     pac::TIM2,
     prelude::*,
     pwm::{PwmChannel, C1, C2, C3},
@@ -22,11 +23,13 @@ use stm32f1xx_hal::{
     timer::{Tim2NoRemap, Timer},
 };
 
-static MODE: AtomicU8 = AtomicU8::new(2);
-static COLOR: AtomicU8 = AtomicU8::new(7);
+static MODE: AtomicU8 = AtomicU8::new(0);
+static COLOR: AtomicU8 = AtomicU8::new(1);
 static mut LED: MaybeUninit<stm32f1xx_hal::gpio::gpioc::PC13<Output<PushPull>>> =
     MaybeUninit::uninit();
-static mut INT_PIN: MaybeUninit<stm32f1xx_hal::gpio::gpioa::PA7<Input<Floating>>> =
+static mut MODE_BUTTON: MaybeUninit<stm32f1xx_hal::gpio::gpioa::PA5<Input<Floating>>> =
+    MaybeUninit::uninit();
+static mut COLOR_BUTTON: MaybeUninit<stm32f1xx_hal::gpio::gpioa::PA4<Input<Floating>>> =
     MaybeUninit::uninit();
 
 #[entry]
@@ -45,11 +48,17 @@ fn main() -> ! {
     //*** PERIPHERALS ***//
     let mut delay = Delay::new(cp.SYST, clocks);
 
-    let int_pin = unsafe { &mut *INT_PIN.as_mut_ptr() };
-    *int_pin = gpioa.pa7.into_floating_input(&mut gpioa.crl);
-    int_pin.make_interrupt_source(&mut afio);
-    int_pin.trigger_on_edge(&p.EXTI, Edge::RISING);
-    int_pin.enable_interrupt(&p.EXTI);
+    let mode_button = unsafe { &mut *MODE_BUTTON.as_mut_ptr() };
+    *mode_button = gpioa.pa5.into_floating_input(&mut gpioa.crl);
+    mode_button.make_interrupt_source(&mut afio);
+    mode_button.trigger_on_edge(&p.EXTI, Edge::RISING);
+    mode_button.enable_interrupt(&p.EXTI);
+
+    let color_button = unsafe { &mut *COLOR_BUTTON.as_mut_ptr() };
+    *color_button = gpioa.pa4.into_floating_input(&mut gpioa.crl);
+    color_button.make_interrupt_source(&mut afio);
+    color_button.trigger_on_edge(&p.EXTI, Edge::RISING);
+    color_button.enable_interrupt(&p.EXTI);
 
     let pa0 = gpioa.pa0.into_alternate_push_pull(&mut gpioa.crl);
     let pa1 = gpioa.pa1.into_alternate_push_pull(&mut gpioa.crl);
@@ -59,7 +68,7 @@ fn main() -> ! {
         &mut afio.mapr,
         1.khz(),
     );
-    //enable for debugging
+
     let mut gpioc = p.GPIOC.split(&mut rcc.apb2);
     let led = unsafe { &mut *LED.as_mut_ptr() };
     *led = gpioc.pc13.into_push_pull_output(&mut gpioc.crh);
@@ -68,9 +77,12 @@ fn main() -> ! {
     let mut nvic = cp.NVIC;
     unsafe {
         nvic.set_priority(EXTI9_5, 1);
+        nvic.set_priority(EXTI4, 1);
         cortex_m::peripheral::NVIC::unmask(EXTI9_5);
+        cortex_m::peripheral::NVIC::unmask(EXTI4);
     }
     cortex_m::peripheral::NVIC::unpend(EXTI9_5);
+    cortex_m::peripheral::NVIC::unpend(EXTI4);
 
     //*** VARS ***//
     let mut pwm_channels = pwm.split();
@@ -86,7 +98,9 @@ fn main() -> ! {
         } else if mode == 2 {
             pulse_colors(mode, &mut pwm_channels, &mut delay);
         } else if mode == 1 {
+            const_color(mode, &mut pwm_channels);
         } else {
+            const_colors(mode, &mut pwm_channels, &mut delay);
         }
     }
 }
@@ -95,13 +109,27 @@ fn main() -> ! {
 fn EXTI9_5() {
     let mut mode = MODE.load(Ordering::Relaxed);
     let led = unsafe { &mut *LED.as_mut_ptr() };
-    let int_pin = unsafe { &mut *INT_PIN.as_mut_ptr() };
+    let mode_button = unsafe { &mut *MODE_BUTTON.as_mut_ptr() };
 
-    if int_pin.check_interrupt() {
+    if mode_button.check_interrupt() {
         led.toggle().unwrap();
         mode = (mode + 1) % 4;
         MODE.store(mode, Ordering::Relaxed);
-        int_pin.clear_interrupt_pending_bit();
+        mode_button.clear_interrupt_pending_bit();
+    }
+}
+
+#[interrupt]
+fn EXTI4() {
+    let led = unsafe { &mut *LED.as_mut_ptr() };
+    let color_button = unsafe { &mut *COLOR_BUTTON.as_mut_ptr() };
+    let mut color = COLOR.load(Ordering::Relaxed);
+
+    if color_button.check_interrupt() {
+        led.toggle().unwrap();
+        color = (color + 1) % 8;
+        COLOR.store(color, Ordering::Relaxed);
+        color_button.clear_interrupt_pending_bit();
     }
 }
 /// Pulses a single color, if the value of COLOR changes this will change
@@ -114,7 +142,7 @@ fn pulse_color(
     ),
     delay: &mut Delay,
 ) {
-    let max = channels.0.get_max_duty() / 4;
+    let max = channels.0.get_max_duty();
     let min = 0;
     let mut duty_cycle = min;
     let mut to_add = true;
@@ -136,27 +164,27 @@ fn pulse_color(
                 to_add = true;
             }
         }
-        
+
         if color & 1 == 1 {
             channels.0.set_duty(duty_cycle);
-        }
-        else {
+        } else {
             channels.0.set_duty(min);
         }
-        if color & 2 == 2{
+        if color & 2 == 2 {
             channels.1.set_duty(duty_cycle);
-        }
-        else {
+        } else {
             channels.1.set_duty(min);
         }
         if color & 4 == 4 {
             channels.2.set_duty(duty_cycle);
-        }
-        else {
+        } else {
             channels.2.set_duty(min);
         }
         delay.delay_ms(1_u16);
     }
+    channels.0.set_duty(min);
+    channels.1.set_duty(min);
+    channels.2.set_duty(min);
 }
 /// Changes color after every pulse, changing the value of COLOR will change immediately
 fn pulse_colors(
@@ -168,7 +196,7 @@ fn pulse_colors(
     ),
     delay: &mut Delay,
 ) {
-    let max = channels.0.get_max_duty() / 4;
+    let max = channels.0.get_max_duty();
     let min = 0;
     let mut duty_cycle = min;
     let mut to_add = true;
@@ -195,25 +223,89 @@ fn pulse_colors(
                 to_add = true;
             }
         }
-        
+
         if color & 1 == 1 {
             channels.0.set_duty(duty_cycle);
-        }
-        else {
+        } else {
             channels.0.set_duty(min);
         }
-        if color & 2 == 2{
+        if color & 2 == 2 {
             channels.1.set_duty(duty_cycle);
-        }
-        else {
+        } else {
             channels.1.set_duty(min);
         }
         if color & 4 == 4 {
             channels.2.set_duty(duty_cycle);
-        }
-        else {
+        } else {
             channels.2.set_duty(min);
         }
-        // delay.delay_ms(1_u16);
+        delay.delay_ms(1_u16);
+    }
+    channels.0.set_duty(min);
+    channels.1.set_duty(min);
+    channels.2.set_duty(min);
+}
+
+///Display a constant color which can be adjust by the color button
+fn const_color(
+    mode: u8,
+    channels: &mut (
+        PwmChannel<TIM2, C1>,
+        PwmChannel<TIM2, C2>,
+        PwmChannel<TIM2, C3>,
+    ),
+) {
+    let max = channels.0.get_max_duty();
+    let min = 0;
+    while MODE.load(Ordering::Relaxed) == mode {
+        let color = COLOR.load(Ordering::Relaxed);
+        if color & 1 == 1 {
+            channels.0.set_duty(max);
+        } else {
+            channels.0.set_duty(min);
+        }
+        if color & 2 == 2 {
+            channels.1.set_duty(max);
+        } else {
+            channels.1.set_duty(min);
+        }
+        if color & 4 == 4 {
+            channels.2.set_duty(max);
+        } else {
+            channels.2.set_duty(min);
+        }
+    }
+}
+
+///Display a constant color which can be adjust by the color button
+//TODO: add a delay in color change in a nonblocking way so the color button still works
+fn const_colors(
+    mode: u8,
+    channels: &mut (
+        PwmChannel<TIM2, C1>,
+        PwmChannel<TIM2, C2>,
+        PwmChannel<TIM2, C3>,
+    ),
+    delay: &mut Delay,
+) {
+    let max = channels.0.get_max_duty();
+    let min = 0;
+    while MODE.load(Ordering::Relaxed) == mode {
+        let color = COLOR.load(Ordering::Relaxed);
+        if color & 1 == 1 {
+            channels.0.set_duty(max);
+        } else {
+            channels.0.set_duty(min);
+        }
+        if color & 2 == 2 {
+            channels.1.set_duty(max);
+        } else {
+            channels.1.set_duty(min);
+        }
+        if color & 4 == 4 {
+            channels.2.set_duty(max);
+        } else {
+            channels.2.set_duty(min);
+        }
     }
 }
